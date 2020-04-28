a434958d77d0237c7b7c82141f71b9287cae98e3
Simon Kalmar Riis Mortensen <simonkalmar@gmail.com>
2020-04-28 22:31:15 +0200

    opbygning

---
+++ site/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const idx = require("../controllers/indexController");
+
+router.get('/', forwardAuthenticated, idx.frontpage);
+
+router.get('/dashboard', ensureAuthenticated, idx.dashboard);
+
+module.exports = router;
