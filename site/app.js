22aedbff8b539d1c65eb560c036f17743d921545
Jonatan Shoshan <jonatan-bs@live.dk>
2020-04-29 09:55:53 +0200

    removed views and changed dbName to yaddayaddayadda

--- site/app.js
+++ site/app.js
@@ -1,44 +1,52 @@
-const createError = require('http-errors');
-const path = require('path');
-const logger = require('morgan');
+const createError = require("http-errors");
+const path = require("path");
+const logger = require("morgan");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // DB Config and server connect
-const db = require('./config/keys').mongoURI;
-mongoose.connect('mongodb://localhost/node-auth', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    })
-    .then( function() { console.log('mongoose connection open'); })
-    .catch( function(err) { console.error(err); });
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect("mongodb://localhost:27017/yaddayaddayadda", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(function () {
+    console.log("mongoose connection open");
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
 
 const app = express();
-app.locals.pretty = app.get('env') === 'development';       // pretty print html
+app.locals.pretty = app.get("env") === "development"; // pretty print html
 
 // view engine setup pug and static
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Express session
-app.use(require('express-session')({                        // passport initialize
-    secret: 'ioeruir!rznbzvmn8768576hdsw&%',                 // do the keyboard cat
-    resave: true,                                           // to create entropy
-    saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    // passport initialize
+    secret: "ioeruir!rznbzvmn8768576hdsw&%", // do the keyboard cat
+    resave: true, // to create entropy
+    saveUninitialized: false,
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -48,16 +56,15 @@ app.use(passport.session());
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
 // Routes
-app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
+app.use("/api/users", require("./routes/users.js"));
 
 /*
 // catch 404 and forward to error handler
