05c70f9b68725744073277fa9be9a3ca0bcdd82c
Jonatan Shoshan <jonatan-bs@live.dk>
2020-05-12 12:11:51 +0200

    follow bug fixed

--- models/User.js
+++ models/User.js
@@ -1,50 +1,80 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  confirmed: {
-    type: Boolean,
-    default: false,
+const options = {
+  toObject: {
+    virtuals: true,
   },
-  firstname: {
-    type: String,
-    required: true,
+  toJSON: {
+    virtuals: true,
   },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  following: [
-    {
+};
+
+const UserSchema = new mongoose.Schema(
+  {
+    confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    firstname: {
+      type: String,
+      required: true,
+      set: (v) => v.toLowerCase(),
+    },
+    lastname: {
+      type: String,
+      required: true,
+      set: (v) => v.toLowerCase(),
+    },
+    image: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    following: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      set: (v) => v.toLowerCase(),
+    },
+    password: {
       type: String,
       required: true,
     },
-  ],
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    darkTheme: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  options
+);
+UserSchema.virtual("followers", {
+  ref: "User",
+  localField: "_id",
+  foreignField: "following",
+});
+// UserSchema.virtual("followers").get(function () {
+//   User.find({ following: { $in: this._id } }, null, function (followers) {
+//     return "followers";
+//   });
+//    return "kjlkjk";
+// });
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  darkTheme: {
-    type: Boolean,
-    default: false,
-  },
+UserSchema.virtual("fullname").get(function () {
+  return `${this.firstname} ${this.lastname}`;
 });
 
 const User = mongoose.model("User", UserSchema, "user");
